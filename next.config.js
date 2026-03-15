c0ea7939e6f086b9e753ad490a206cc2fa885d62
FAMZ\oatza <phuminan.w@gmail.com>
2026-03-15 17:16:49 +0700

    Fix GitHub Pages deployment: use next.config.js and simplify workflow

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/medicare-link',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
